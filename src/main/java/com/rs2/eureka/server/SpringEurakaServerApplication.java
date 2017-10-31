9cdb441be62da144e0db52dea0e30024f79958d2
Rajesh Kumar Somarppa <rsomarppa@Rajeshs-MacBook-Pro.local>
2017-10-30 22:11:48 -0500

    initial version
    initial version

---
+++ src/main/java/com/rs2/eureka/server/SpringEurakaServerApplication.java
@@ -0,0 +1,14 @@
+package com.rs2.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringEurakaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurakaServerApplication.class, args);
+	}
+}
